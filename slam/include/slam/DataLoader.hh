71a698aa96ee5948d80408c379c9dd0de25f3deb
Seth Isaacson <sisaacson@g.hmc.edu>
2020-12-21 20:48:49 -0500

    barebones files

---
+++ slam/include/slam/DataLoader.hh
@@ -0,0 +1,18 @@
+#pragma once
+
+#namespace MuddSub::SLAM
+{
+
+class DataLoader
+{};
+
+
+
+
+
+
+
+
+
+
+};
---
+++ slam/include/slam/FastSLAM.hh
@@ -0,0 +1,10 @@
+#pragma once
+
+#namespace MuddSub::SLAM
+{
+
+class FastSLAM
+{};
+
+
+};
