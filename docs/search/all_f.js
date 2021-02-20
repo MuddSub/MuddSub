fd933cfe76556d88b538ce23c9949941260b212d
Christopher Herrera <ciherrera20@icloud.com>
2021-02-20 00:33:44 -0800

    Removed doxygen generated files and added the docs repository to the gitignore

--- docs/search/all_f.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['update_44',['update',['../classPidController.html#aad1e3538777c6e172198d2fc2d3d2cf0',1,'PidController']]]
-];
--- docs/search/classes_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['vehicledynamics_58',['VehicleDynamics',['../classVehicleDynamics_1_1VehicleDynamics.html',1,'VehicleDynamics.VehicleDynamics'],['../classMuddSub_1_1Controls_1_1VehicleDynamics.html',1,'MuddSub::Controls::VehicleDynamics']]]
-];
