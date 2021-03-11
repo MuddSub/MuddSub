6977485a96c494073d29185d95e6d1ab1cf6cbca
Alex Fay <43191440+Alex-Fay@users.noreply.github.com>
2021-03-11 11:59:38 -0800

    fixed var documentation, add dynamic matrix
    fixed from after Wednesday night meeting

--- controls/src/MPC.py
+++ controls/src/MPC.py
@@ -1,76 +1,78 @@
 '''
- TODO: give examples of how this works for us
+Overview of MPC variables:
+ Ad: a dynamic matrix of the robots state variables (i.e. center of gravity(velocity) + intertia) for each x,y,z,roll, pitch, yaw, and change in linear and angular velocity
+ Bd: The 8 thrusters of the robot with vecotrized forces from each
+ Q: The associated weights of each function to minimize error (i.e. how much weight error in z dimension (depth) has over error in y(range))
 
- initialize Ad path plan from navigation
- initialize Bd Model of our future prediction
- nx and nu are dimensions of Bd
- Q and QN are diagonal matrices
- objective function is the integral of the error between navigation and future prediction
- ?(R is 0.1*I^4)
+ Model constraints
+ umin: max reverse force of thrusters in kg(f), minimum control effort
+ umax: max forward force of thrusters in kg(f), maximum control effort
+ xmin: minimum output for all states in order of roll pitch yaw x y z, and velocities
+ xmax: maximum otuput for all states in order of roll pitch yaw x y z, and velocities
+ nsim: number of times model optimizes
 
- constraints
- umin: minimum control effort
- umax: maximum control effort
- xmin: minimum x value (array for all states)
- xmax: maximum x value (array for all states)
+ x0: plantState from slam (pose)
+ xr: setState from trajectory
+ N: prediction horizon - number of steps MPC looks into the future
+ P: diagnal matrix with weights of model (Q), entries of the minimized error matrix Q repeated (N+1) times and 0.1 (4*N) times.
 
+ Matrix Math Overview:
+ Intermediate Q stacks arrays of -Q.(xr), and adds zeros at the end
+ Ax krons a matrix of 1s and 0s with Ad, our model of our path plan from navigation
+ Bu stacks a bunch of matrics and krons the result with Bd, our future prediction
+ Aeq hstacked Ax and Bu
+
+ Main function overview:
+ loop
+ res = prob.solve()
+ ctrl = res.x[-N*nu:-(N-1)*nu]
+ x0 = Ad.dot(x0) + Bd.dot(ctrl), updates current position
+ updates position and control effort into our initial states for mpc
+
+ROS Subscribers and Nodes:
  #Inputs
  self.trajectorySub = rospy.Subscriber("/controls/trajectory", Path, self.trajectoryCB) #navigation's trajectory
  self.poseSub = rospy.Subscriber("slam/robot/pose", Odometry, self.poseCB) #ask if msg type slam is /pose or /state
 
  #Outputs
  self.wrenchPub = rospy.Publisher("/controls/robot/wrench",geometry_msgs.msg.WrenchStamped, queue_size= 1) #total thruster output force and torque
-
- x0 is the initial state in (x, y, z, roll, pitch, yaw, x', y', z', roll', pitch', yaw')
-
- xr is the reference state in the same form
-
- N is the prediction horizon: 10
-
- P is a diagonal matrix with the diagonal entries of the minimized error matrix Q repeated
- (N+1) times and 0.1 (4*N) times.
- Q stacks arrays of -Q.(xr), and adds zeros at the end
- Ax krons a matrix of 1s and 0s with Ad, our model of our path plan from navigation
- Bu stacks a bunch of matrics and krons the result with Bd, our future prediction
- Aeq hstacked Ax and Bu
-
- nsim is the number of times we optimize, while N is how far into the future we look
-
-
-
- loop
- res = prob.solve()
- ctrl = res.x[-N*nu:-(N-1)*nu]
- x0 = Ad.dot(x0) + Bd.dot(ctrl), updates current position
- updates position and control effort into our initial states for mpc
 '''
-
 
 import numpy as np
 import scipy as sp
 from scipy import sparse
-import osqp
-dt = 0.1
+import osqp #matrix optimizer
 
-#we need to figure this out better
-#(roll, pitch, yaw, x, y, z, and all velocities)
-#CG_x = CG(1);
-# CG_y = CG(2);
-# CG_z = CG(3);
-# C_12 = m* [(CG_y*velocity_q + CG_z*velocity_r), -(CG_x*velocity_q - velocity_w), -(CG_x*velocity_r + velocity_v);
-# -(CG_y*velocity_p + velocity_w) , (CG_z*velocity_r + CG_x*velocity_p), -(CG_y*velocity_r - velocity_u);
-# -(CG_z*velocity_p - velocity_v), -(CG_z*velocity_q + velocity_u), (CG_x*velocity_p+CG_y*velocity_q)];
-# C_22 = [0, -Iyz*velocity_q-Ixz*velocity_p+Izz*velocity_r, Iyz*velocity_r+Ixy*velocity_p-Iyy*velocity_q;
-# Iyz*velocity_q+Ixz*velocity_p-Izz*velocity_r, 0, -Ixz*velocity_r-Ixy*velocity_q+Ixx*velocity_p;
-# -Iyz*velocity_r-Ixy*velocity_p-Iyy*velocity_q, Ixz*velocity_r+Ixy*velocity_q-Ixx*velocity_p, 0];
-#
-#
-# C_a = ...
-# [ 0, 0, 0, 0, -M_aZ*velocity_w, M_aY*velocity_v;
-# 0, 0, 0, M_aZ*velocity_w, 0, -M_aX*velocity_u;
-# 0, 0, 0, -M_aY*velocity_v, -M_aX*velocity_u, 0;
-# 0, -M_aZ*velocity_w, -M_aY*velocity_v, 0, -M_ay*velocity_r, -M_ap*velocity_q;
-# M_aZ*velocity_w, 0, -M_aX*velocity_u, -M_ay*velocity_
+"""
+#TODO: change to real values of robot, current guesses
+dt = 0.1 #change in time between steps
+robot_mass = 52 #kg, replace with real weight
+CG_x, CG_y, CG_z = 0,0, -.1
+vel_x, vel_y, vel_z, vel_r, vel_p, vel_yaw = 1,0,0,0,0,0 #change to input from dvl?
+Ixx, Ixy, Ixz, Iyy, Iyz, Izz = 1,1,.5,0,1,.25 #ake inertia adjustable
+M_aX, M_aY, M_aZ, M_ar, M_ap, M_ay = 1,.25,0,0,0,0 #addedc mass coef
+
+C_CG = robot_mass* [(CG_y*vel_p + CG_z*vel_yaw), -(CG_x*vel_p - vel_z), -(CG_x*vel_yaw + vel_y),
+          -(CG_y*vel_r + vel_z) , (CG_z*vel_yaw + CG_x*vel_r), -(CG_y*vel_yaw - vel_x),
+          -(CG_z*vel_r - vel_y), -(CG_z*vel_p + vel_x), (CG_x*vel_r+CG_y*vel_p)]
+
+C_Interia = [0, -Iyz*vel_p-Ixz*vel_r+Izz*vel_yaw, Iyz*vel_yaw+Ixy*vel_r-Iyy*vel_p,
+           Iyz*vel_p+Ixz*vel_r-Izz*vel_yaw, 0, -Ixz*vel_yaw-Ixy*vel_p+Ixx*vel_r,
+           -Iyz*vel_yaw-Ixy*vel_r-Iyy*vel_p, Ixz*vel_yaw+Ixy*vel_p-Ixx*vel_r, 0]          
+
+C_a = [ 0, 0, 0, 0, -M_aZ*vel_z, M_aY*vel_y,
+        0, 0, 0, M_aZ*vel_z, 0, -M_aX*vel_x,
+        0, 0, 0, -M_aY*vel_y, -M_aX*vel_x, 0,
+        0, -M_aZ*vel_z, -M_aY*vel_y, 0, -M_ay*vel_yaw, -M_ap*vel_p,
+        M_aZ*vel_z, 0, -M_aX*vel_x, -M_ay*vel_yaw, 0, -M_ar*vel_r,
+        -M_aY*vel_y, M_aX*vel_x, 0, -M_ap*vel_p, -M_ar * vel_r, 0]
+
+C_rb = [np.zeros(3), C_CG, -np.transpose(C_CG), C_Interia]
+    
+Ad = sparse.csc_matrix((C_a + C_rb)) #fix sparese matrix (maybe numpy add?)
+"""
+
+#A temporary matrix of the correct dimensions of models dynamics
 Ad = sparse.csc_matrix([
   [1.,      0.,     0., 0., 0., 0., dt,     0.,     0.,  0.,      0.,     0.    ],
   [0.,      1.,     0., 0., 0., 0., 0.,      dt,    0.,  0.,      0.,     0.    ],
@@ -86,7 +88,7 @@ Ad = sparse.csc_matrix([
   [0.,      0.,     0., 0., 0., 0., 0.,      0.,     0.,  0.,     0.,     1.    ]
 ])
 
-#Control dynamics matrix. Get from Max. Columns are thrusters, rows are state variables.
+#vectorized thruster forces, each column is a thruster
 Bd = sparse.csc_matrix([
   [0.,      -0.0726,  0.,     0.0726,0.,      -0.0726,  0.,     0.0726],
   [-0.0726,  0.,      0.0726, 0.    ,-0.0726,  0.,      0.0726, 0.    ],
@@ -100,18 +102,16 @@ Bd = sparse.csc_matrix([
   [-0.,     -0.0236,  0.,     0.0236,-0.,     -0.0236,  0.,     0.0236],
   [0.0236,   0.,     -0.0236, 0.    ,0.0236,   0.,     -0.0236, 0.    ],
   [0.2107,   0.2107,  0.2107, 0.2107,0.2107,   0.2107,  0.2107, 0.2107]])
-#nx is number of states, nu is number of thrusters
-[nx, nu] = Bd.shape
+
+[nx, nu] = Bd.shape #nx is number of states, nu is number of thrusters
 
 #Constraints in kgf = 9.81kgN
-umin = np.array([-4.1, -4.1, -4.1, -4.1, -4.1, -4.1, -4.1, -4.1])
-umax = np.array([5.25, 5.25, 5.25, 5.25, 5.25, 5.25, 5.25, 5.25])
-
-#roll pitch yaw x y z, and velocities
+umin = np.array([-4.1, -4.1, -4.1, -4.1, -4.1, -4.1, -4.1, -4.1]) #reverse thruster power
+umax = np.array([5.25, 5.25, 5.25, 5.25, 5.25, 5.25, 5.25, 5.25]) #forward thruster power
 xmin = np.array([-np.pi/6,-np.pi/6,-np.inf,-np.inf,-np.inf,0.1,
-                 -np.inf,-np.inf,-np.inf,-np.inf,-np.inf,-np.inf])
+                 -np.inf,-np.inf,-np.inf,-np.inf,-np.inf,-np.inf]) #[roll pitch yaw x y z, and linear and angular velocities]
 xmax = np.array([ np.pi/6, np.pi/6, np.inf, np.inf, np.inf, 10,
-                  np.inf, np.inf, np.inf, np.inf, np.inf, np.inf])
+                  np.inf, np.inf, np.inf, np.inf, np.inf, np.inf]) #[roll pitch yaw x y z, and linear and angular velocities]
 
 #how much we care about being off of our target state for each state variable
 #roll pitch yaw x y z and their respective velocities
@@ -168,11 +168,13 @@ for i in range(nsim):
 
     # Apply first control input to the plant
     ctrl = res.x[-N*nu:-(N-1)*nu]
-    x0 = Ad.dot(x0) + Bd.dot(ctrl)
+    x0 = Ad.dopip
+    
+    t(x0) + Bd.dot(ctrl)
 
     # Update initial state
     l[:nx] = -x0
     u[:nx] = -x0
     prob.update(l=l, u=u)
     print(i)
-print("hello")
+
