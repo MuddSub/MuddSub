9e3380d4d131e979fa0f1b1038a81b81254d305c
tanvikad <tanvika.dasari@gmail.com>
2021-03-28 19:03:35 -0700

    Rearranged folders, and added draft AStar in c++

---
+++ navigation/include/navigation/AStar.hh
@@ -0,0 +1,56 @@
+
+#pragma once
+#include <navigation/AStar.cc>
+#include <limits>
+#include <math.h>
+namespace MuddSub::Navigation
+class AStar
+{
+private:
+    double*** distance_;
+    int width_;
+    int height_;
+    int depth_;
+    double prob_;
+    int startx_;
+    int starty_;
+    int startz_;
+    int endx_;
+    int endy_;
+    int endz_;
+    void makeGrid();
+    void printGrid();
+    void solveGrid();
+    void solveGridDFS();
+    void DFS(int x, int y, int z);
+    class Node
+    {
+    public:
+        static double infinity;
+        bool isOpen_ = false;
+        bool isClosed_ = false;
+        bool isObstacle_ = false;
+        int x_;
+        int y_;
+        int z_;
+        double f_ = infinity;
+        double  g_ = infinity;
+        double  h_ = 0;
+        std::string str_  = "0";
+        Node* parent_ = NULL;
+        Node(int x, int y, int z, Node *end);
+        Node();
+        void setObstacle(bool isObstacle);
+        friend bool operator< (const Node& a, const Node& b){return a.f_ > b.f_;}
+        friend bool operator> (const Node& a, const Node& b) {return a.f_ > b.f_;}
+        //operator std::string() const {return str_;}
+    };
+public:
+    Node*** grid_;
+    AStar(int width, int height, int depth, double prob, int startx, int starty, int startz, int endx, int endy, int endz);
+    AStar(int width, int height, int depth);
+    void makeParent(Node *node);
+    //double static distance (int x1, int y1, int z1, int x2, int y2, int z2);
+};
+}
+
