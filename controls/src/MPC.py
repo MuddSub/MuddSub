e87f0acad778529e42c89580f4948ec9a6281cc4
Alex Fay <43191440+Alex-Fay@users.noreply.github.com>
2021-04-04 19:18:46 -0700

    Dynamic Matrix output from ros node
    in progress

--- controls/src/MPC.py
+++ controls/src/MPC.py
@@ -30,29 +30,90 @@ ROS Subscribers and Nodes:
  self.poseSub = rospy.Subscriber("slam/robot/pose", Odometry, self.poseCB) #ask if msg type slam is /pose or /state
  #Outputs
  self.wrenchPub = rospy.Publisher("/controls/robot/wrench",geometry_msgs.msg.WrenchStamped, queue_size= 1) #total thruster output force and torque
-'''
 
+ Compute Matrix         0
+         0
+  -6094.16
+         0
+         0
+         0
+         0
+         0
+3.0471e+06
+         0
+         0
+         0       0
+       0
+ 12.1881
+       0
+       0
+       0
+       0
+       0
+-6094.16
+       0
+       0
+       01.61757e+09
+[ INFO] [1617572548.096853897]: Time: -1.001985
+[ INFO] [1617572549.085581283]: Iterating simulator
+STATE: 
+       0
+       0
+ 12.1881
+       0
+       0
+       0
+       0
+       0
+-6094.16
+       0
+       0
+       0
+DERIV: 
+         0
+         0
+  -6094.16
+         0
+         0
+         0
+         0
+         0
+3.0471e+06
+         0
+         0
+         0
+
+roscore 
+rosrun xacro xacro ../../core/descriptions/alfie.urdf.xacro -o ../../core/descriptions/alfie.urdf 
+rosrun controls simulation_dynamics
+'''
 import numpy as np
 import scipy as sp
 from scipy import sparse
 import osqp #matrix optimizer
-import vehicleDynamics
-
+import scripts/vehicleDynamics
 """
 //https://github.com/MuddSub/MuddSub/blob/develop/controls/controls/scripts/VehicleDynamics.py
 //read in yaml file: controls/cfg/dynamics.yaml from ros?
 //code review ask what rate means
-
 //linearizedSystem = control.linearize(self.nonlinearIoSys, self.x, self.u)
 //dt = a/self.rate
 Taken from: https://github.com/MuddSub/MuddSub/blob/develop/base/controls/cfg/dynamics.yaml
-
 """
 rate = 50
-dt = 1/rate 
+dt = 1/rate
 mass = 20 #kg
-
+MPC_matrix = VehicleDynamics();
 #A temporary matrix of the correct dimensions of models dynamics
+
+
+Ad = sparse.csc_matrix([ #Figure out wtf this matrix is next week
+  [0, 0, 12.2132, 0, 0, 0],
+  [ 0, 0, -6106.65, 0, 0, 0],
+  [0, 0, -0.0245242, 0, 0, 0]
+  [0, 0, 12.2132, 0, 0, 01.61757e+09]
+])
+
 Ad = sparse.csc_matrix([
   [1.,      0.,     0., 0., 0., 0., dt,     0.,     0.,  0.,      0.,     0.    ],
   [0.,      1.,     0., 0., 0., 0., 0.,      dt,    0.,  0.,      0.,     0.    ],
@@ -67,7 +128,6 @@ Ad = sparse.csc_matrix([
   [0.,     -0.9734, 0., 0., 0., 0., 0.,     -0.0488, 0.,  0.,     1.,     0.    ],
   [0.,      0.,     0., 0., 0., 0., 0.,      0.,     0.,  0.,     0.,     1.    ]
 ])
-
 #vectorized thruster forces, each column is a thruster
 #Wrench x y z r p y
 Bd = sparse.csc_matrix([
@@ -83,9 +143,7 @@ Bd = sparse.csc_matrix([
   [dt/mass,         0.,              0.,              0.,              0.,              0.             ], #x'
   [0.,              dt/mass,         0.,              0.,              0.,              0.             ], #y'
   [0.,              0.,              dt/mass,         0.,              0.,              0.             ]])#z'
-
 [nx, nu] = Bd.shape #nx is number of states, nu is number of thrusters
-
 #Constraints in kgf = 9.81kgN
 umin = np.array([-4.1, -4.1, -4.1, -4.1, -4.1, -4.1]) #reverse thruster power
 umax = np.array([5.25, 5.25, 5.25, 5.25, 5.25, 5.25]) #forward thruster power
@@ -93,22 +151,17 @@ xmin = np.array([-np.inf,-np.inf,-np.inf,-np.inf,-np.inf,0.1,
                  -np.inf,-np.inf,-np.inf,-np.inf,-np.inf,-np.inf]) #[roll pitch yaw x y z, and linear and angular velocities]
 xmax = np.array([ np.inf, np.inf, np.inf, np.inf, np.inf, 10,
                   np.inf, np.inf, np.inf, np.inf, np.inf, np.inf]) #[roll pitch yaw x y z, and linear and angular velocities]
-
 #how much we care about being off of our target state for each state variable
 #roll pitch yaw x y z and their respective velocities
 Q = sparse.diags([100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100])
 QN = Q
 R = sparse.eye(6)
-
 # initial comes from slam and reference comes from navigation
 #TEMPORARY
 x0 = np.zeros(12)#input from slam plant state
 xr = np.array([0.,0.,1.,0.,0.,0.,0.,0.,0.,0.,0.,0.]) #input from trajectory
-
 # Prediction horizon. how many steps ahead we are looking
 N = 10
-
-
 # Cast MPC problem to a QP: x = (x(0),x(1),...,x(N),u(0),...,u(N-1))
 # - quadratic objective
 P = sparse.block_diag([sparse.kron(sparse.eye(N), Q), QN,
@@ -130,31 +183,24 @@ uineq = np.hstack([np.kron(np.ones(N+1), xmax), np.kron(np.ones(N), umax)])
 A = sparse.vstack([Aeq, Aineq], format='csc')
 l = np.hstack([leq, lineq])
 u = np.hstack([ueq, uineq])
-
 # Create an OSQP object
 prob = osqp.OSQP()
-
 # Setup workspace
 prob.setup(P, q, A, l, u, warm_start=True)
-
 # Simulate in closed loop
 nsim = 15
 for i in range(nsim):
     # Solve
     res = prob.solve()
-
     # Check solver status
     if res.info.status != 'solved':
         raise ValueError('OSQP did not solve the problem!')
-
     # Apply first control input to the plant
     ctrl = res.x[-N*nu:-(N-1)*nu]
     x0 = Ad.dot(x0) + Bd.dot(ctrl)
-
     x0[0] = x0[0] % (2 * np.pi)
     x0[1] = x0[1] % (2 * np.pi)
     x0[2] = x0[2] % (2 * np.pi)
-
     # Update initial state
     l[:nx] = -x0
     u[:nx] = -x0
