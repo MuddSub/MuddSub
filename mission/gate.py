1958a6e042e74eac8a36c28fc21542e06ee94710
krzxr <rongkyle@gmail.com>
2021-06-07 17:32:42 -0700

    mission architecture

---
+++ mission/gate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import rospy
+import smach
+from std_msgs.msg import Bool, String
+import smach_ros
+
+def gateLoateCB():
+  pass
+
+def gateGoToCB():
+  pass
+
+class GateAction(smach.State):
+  def __init__(self):
+    pass
+
+  def execute(self, ud):
+    pass
---
+++ mission/template_task_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+import smach
+
+# userdata consists of different action takers 
+# (navigators, map_reader) etc
+# locate target keeps track of failures. return abort if > ACCEPTABLE_FAILURE
+# cannot take longer than .5 seconds 
+# (specifically, a time defined to be the required rxn time to respond to kill switch)
+class LocateTarget(smach.State):
+  def __init__(self, task_name):
+    pass
+class GoToTarget(smach.State):
+  def __init__(self, task_name):
+    pass
