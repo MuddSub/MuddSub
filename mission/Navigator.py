e3294597a6cec6fcf82c131f77650ad3a828c79e
Christopher Herrera <ciherrera20@icloud.com>
2021-07-27 19:03:33 -0700

    Created GoToTarget state machine

--- mission/Navigator.py
+++ mission/Navigator.py
@@ -1,7 +1,7 @@
 import rospy
 import smach
-import smach_ros 
-from time import time 
+import smach_ros
+from time import time
 import actionlib
 import navigation.msg
 
@@ -14,46 +14,46 @@ class Navigate(smach.State):
     
     self.initialized = False
     self.timeout = timeout
-    
+
     self.clinet = None
     self.goal = navigation.msg.NavigationAction(goal=goal, measurement = measurement, motion_style = motion_style)
-  
+
   def execute(self, userdata):
     navigator = userdata.navigator
-    
-    # use action server here? 
+
+    # use action server here?
     if not self.initialized:
       navigator.send_goal(self.goal)
       self.initialized = True
       self.startTime = time()
 
     else:
-      
+
       action_status = self.client.get_state()
-      # Possible States Are: PENDING, ACTIVE, RECALLED, 
+      # Possible States Are: PENDING, ACTIVE, RECALLED,
       # REJECTED, PREEMPTED, ABORTED, SUCCEEDED, LOST.
 
       if action_status == SUCCEEDED:
         return 'success'
-      
+
       elif time()-self.startTime <= self.timeout and action_status in [ACTIVE, LOST]:
         if action_status == LOST:
           # to do: log message lost
           navigator.send_goal(self.goal)
           return 'active'
-        
+
         else: return 'abort'
-      
+
       else: return 'abort'
 
 
       '''
       if abs(self.heave.plantState - self.heave.setpoint) < .025:
         self.successCount += 1
-      else: 
+      else:
         self.successCount = 0
-      # use time instead of succesCount?  
-      
+      # use time instead of succesCount?
+
 
       if successCount > self.targetSuccessCount:
         return 'success'
@@ -62,11 +62,11 @@ class Navigate(smach.State):
       else:
         return 'active'
       '''
-  # sample template? 
+  # sample template?
 
   # create goTo template
   # we can template this as goTo template, and add a parallel detection scheme (paralle scheme not necessary if slam works.)
 
-  # interface with navigation: action lib server library, use time to control? navigation says i develop a position? 
+  # interface with navigation: action lib server library, use time to control? navigation says i develop a position?
 
-  # interface with slam. if slam is not working, navigation also not work. 
+  # interface with slam. if slam is not working, navigation also not work.
---
+++ mission/scripts/GoToTarget.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+import rospy
+import smach
+import smach_ros
+
+# define state CheckMap
+class GetTargetLocation(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+            outcomes=['succeeded', 'failed'],
+            input_keys=[],
+            output_keys=[]
+        )
+
+    def execute(self, userdata):
+        return 'succeeded'
+
+# define state GenerateTrajectory
+class GenerateTrajectory(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+            outcomes=['succeeded', 'failed'],
+            input_keys=[],
+            output_keys=[]
+        )
+
+    def execute(self, userdata):
+        return 'succeeded'
+
+# ExecuteTrajectory
+class ExecuteTrajectory(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+            outcomes=['succeeded', 'failed', 'map_changed'],
+            input_keys=[],
+            output_keys=[]
+        )
+
+    def execute(self, userdata):
+        return 'map_changed'
+
+def main():
+    rospy.init_node('go_to_target')
+
+    # Create SMACH state machine
+    sm = smach.StateMachine(outcomes=['target_reached', 'target_not_found', 'generate_trajectory_failed', 'execute_trajectory_failed'])
+
+    # Open the container
+    with sm:
+        # Add states to the container
+        smach.StateMachine.add('GET_TARGET_LOCATION', GetTargetLocation(),
+            transitions={
+                'succeeded': 'GENERATE_TRAJECTORY',
+                'failed': 'target_not_found'
+            },
+            remapping={}
+        )
+        smach.StateMachine.add('GENERATE_TRAJECTORY', GenerateTrajectory(),
+            transitions={
+                'succeeded': 'EXECUTE_TRAJECTORY',
+                'failed': 'generate_trajectory_failed'
+            },
+            remapping={}
+        )
+        smach.StateMachine.add('EXECUTE_TRAJECTORY', ExecuteTrajectory(),
+            transitions={
+                'succeeded': 'target_reached',
+                'failed': 'execute_trajectory_failed',
+                'map_changed': 'GET_TARGET_LOCATION'
+            },
+            remapping={}
+        )
+
+    # Create and start machine sm finished
+    sis = smach_ros.IntrospectionServer('go_to_target_server', sm, 'SM_ROOT/TASK_LAYER/GO_TO_TARGET')
+    sis.start()
+
+    # Execute the state machine
+    outcome = sm.execute()
+
+    # Wait for ctrl-c to stop the application
+    rospy.spin()
+    sis.stop()
+
+if __name__ == '__main__':
+    main()
