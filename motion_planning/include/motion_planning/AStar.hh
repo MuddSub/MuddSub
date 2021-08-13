d14b65bb367422053eeb3de6c1692076a25ca0b4
Tanvika Dasari <tanvika.dasari@gmail.com>
2021-08-12 19:47:46 -0700

    Updated AStar code

--- motion_planning/include/motion_planning/AStar.hh
+++ motion_planning/include/motion_planning/AStar.hh
@@ -12,13 +12,26 @@ class AStar
 public:
     std::vector<std::vector<double>> path_;
     enum rotationalMotion_ {rotationalMovement, sinTraversalPath, None};
-    AStar(int width, int height, int depth, double prob, int startx, int starty, int startz, int endx, int endy, int endz, rotationalMotion_ motion, std::string data);
+    AStar(int width, int height, int depth, double prob, int startx, int starty, int startz, int endx, int endy, int endz, std::string data, double startTime, double velocity);
+    AStar(int*** map, int width, int height, int depth, int startx, int starty, int startz, int endx, int endy, int endz, std::string data, double startTime, double velocity);
+    AStar(std::vector<std::vector<double>> obstacles, int width, int height, int depth, int startx, int starty, int startz, int endx, int endy, int endz, std::string data, double startTime, double velocity);
     AStar(int width, int height, int depth);
+    void updatePose(int startx, int starty, int startz);
+    void solveGrid();
+    void resolveGrid();
+    void solveGridDFS();
+    void printGrid() const;
+    void addObstacles(std::vector<std::vector<double>> obstacle);
+    void addMotion(rotationalMotion_ motion, std::string data);
+    void addTime(int factor); //TODO: Decide between velocity and start time, is factor needed?
+
+
+    static std::vector<std::string>  splitData(std::string data);
+
 
 
 private:
     class Node;
-    double*** distance_;
     int width_;
     int height_;
     int depth_;
@@ -29,29 +42,33 @@ private:
     int endx_;
     int endy_;
     int endz_;
-    //struct pose{ double *position;};
-    Node*** grid_;
-
-    void makeGrid();
-    void printGrid();
-    void solveGrid();
-    void makeParent(Node *node);
-    void solveGridDFS();
-    void DFS(int x, int y, int z);
-    void addRotation(std::string data);
-    void addSinTraversal(std::string data);
-    void addTime();
-
+    int resolveGrid_;
+    double startTime_;
     rotationalMotion_ motion_;
     double velocity_;
+    //struct pose{ double *position;};
+    Node*** grid_;
+    double*** distance_;
+
+    void makeGridFromMap(int*** map);
+    void makeGridFromObstacles(std::vector<std::vector<double>>);
+    void makeGrid();
+
+
+    void makeParent(Node *node, std::string value);
+    void DFS(int x, int y, int z);
+    void addRotation(std::string data);
+    void addSinTraversal(int amp, int freq, int period);
+
+
+
+
 
     class Node
     {
 
     public:
         static double infinity;
-        bool isOpen_ = false;
-        bool isClosed_ = false;
         bool isObstacle_ = false;
         int x_;
         int y_;
@@ -60,6 +77,7 @@ private:
         double  g_ = infinity;
         double  h_ = 0;
         std::string str_  = "0";
+        std::string id_;
         Node* parent_ = NULL;
         rotationalMotion_ motion_;
 
@@ -68,6 +86,8 @@ private:
         void setObstacle(bool isObstacle);
         friend bool operator< (const Node& a, const Node& b){return a.f_ > b.f_;}
         friend bool operator> (const Node& a, const Node& b) {return a.f_ <b.f_;}
+
+
 
         template<typename T>
         bool operator()(T *a, T *b) {
