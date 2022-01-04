503c4ab7f6a43b9eb28dd563bcfebba7a3fdde58
echen4628 <echen4628@gmail.com>
2022-01-04 15:07:54 -0800

    proof read gate_protocol code
    Co-authored-by: StephanieFulcar <StephanieFulcar@users.noreply.github.com>

--- vision/scripts/gate_protocol.py
+++ vision/scripts/gate_protocol.py
@@ -2,9 +2,10 @@
 import numpy as np
 import rospy
 from vision.msg import DetectionArray
+import time
 class Gate_Protocol():
-    CAMERA_FOV = 2
-    CAMERA_XRES = 416
+    CAMERA_FOV = 2 # check with our camera
+    CAMERA_XRES = 416 # check with our camera
     NUM_SET_POINTS = 72
     FORWARD_STEP_SIZE = 1
 
@@ -22,14 +23,19 @@ class Gate_Protocol():
                 self.gateDetection = detection
                 FOUND_GATE = True
         self.range_to_gate = self.gateDetection.range if FOUND_GATE else None
-        self.gate_width = self.gateDetection.boundingBox.x * 2
+        self.gate_width = self.gateDetection.boundingBox.x * 2 if FOUND_GATE else self.gate_width
         
     def main(self):
-        self.spin()
-        direction = self.shimmy()
-        if direction != 0:  
-            self.orbit()
-        self.forward
+        # check if self.gate_width is not None
+
+        success = self.spin()
+        if success:
+            direction = self.shimmy()
+            if direction:  #fix returns
+                self.orbit()
+            self.forward()
+        else:
+            return False
 
     def spin(self, obstacle_name):
         # CONTROLS
@@ -41,30 +47,53 @@ class Gate_Protocol():
 
         # subscribe to the detected objects
         # search though the subscribed objects until we find the object with name obstacle_name
-        OBS_X = self.detectionArray.boundingBox.center.x
+
+        starting_time = time.time()
+
+        while self.detectionArray == None:
+            current_time = time.time()
+            if current_time - starting_time > 30:
+                return False
+            print("Cannot find Gate")
+        OBS_X = self.detectionArray.boundingBox.center.x # pixel vs normalized 0 - 1
         theta = Gate_Protocol.CAMERA_FOV * (OBS_X/Gate_Protocol.X_RES - 0.5)
         # +++ controls --> spin by theta
+        return True
         # blocking
     
     def generateSetPoints(self):
         # +++ controls --> set current position as origin
-        r = self.range_to_gate
+        r = self.range_to_gate #this is in the x direction
+        # check if 1 is inclusive 
         N = np.arange(0,1,Gate_Protocol.NUM_SET_POINTS)
+        # self.set_points = r*np.array([
+        #     r * (1-np.cos(2*np.pi*N)), 
+        #     r * (np.sin(2*np.pi*N)),
+        #     np.zeros(Gate_Protocol.NUM_SET_POINTS), # same z position
+        #     np.zeros(Gate_Protocol.NUM_SET_POINTS), # no rotation around x
+        #     np.zeros(Gate_Protocol.NUM_SET_POINTS), # no rotation around y
+        #     -2*np.pi*N, # rotation around z counterclockwise
+        #     np.zeros(Gate_Protocol.NUM_SET_POINTS),np.zeros(Gate_Protocol.NUM_SET_POINTS),
+        #     np.zeros(Gate_Protocol.NUM_SET_POINTS),np.zeros(Gate_Protocol.NUM_SET_POINTS),
+        #     np.zeros(Gate_Protocol.NUM_SET_POINTS),np.zeros(Gate_Protocol.NUM_SET_POINTS) # all zero velocities
+        #     ]).reshape(-1, 12)
         self.set_points = r*np.array([
             r * (1-np.cos(2*np.pi*N)), 
             r * (np.sin(2*np.pi*N)),
-            0, # same z position
-            0, # no rotation around x
-            0, # no rotation around y
-            -2*np.pi*N, # rotation around z
-            0,0,0,0,0,0 # all zero velocities
-            ]).reshape(-1, 12)
-
+            np.zeros(Gate_Protocol.NUM_SET_POINTS), # same z position
+            np.zeros(Gate_Protocol.NUM_SET_POINTS), # no rotation around x
+            np.zeros(Gate_Protocol.NUM_SET_POINTS), # no rotation around y
+            -2*np.pi*N, # rotation around z counterclockwise
+            np.zeros(Gate_Protocol.NUM_SET_POINTS),np.zeros(Gate_Protocol.NUM_SET_POINTS),
+            np.zeros(Gate_Protocol.NUM_SET_POINTS),np.zeros(Gate_Protocol.NUM_SET_POINTS),
+            np.zeros(Gate_Protocol.NUM_SET_POINTS),np.zeros(Gate_Protocol.NUM_SET_POINTS) # all zero velocities
+            ])
+        self.set_points = np.column_stack(self.set_points)
 
     def shimmy(self):
         ''' returns True if we need to orbit, returns False if we do not need to orbit
             (i.e. we're already in front of the gate)'''
-        self.generateSetPoints()
+        self.generateSetPoints() #clockwise rotation
         original_width = self.gate_width
         # controls --> goto self.set_points[5] # going left
         width_left = self.gate_width
@@ -78,7 +107,7 @@ class Gate_Protocol():
             orbit_direction = 0
         elif width_left > original_width and width_right > original_width:
             orbit_direction = +1 # either direction
-        # controls --> goto self.set_points[0] # going right
+        # controls --> goto self.set_points[0] # going back to the starting position
         if orbit_direction < 0:
             self.set_points = np.flip(self.set_points, axis=0)
             return True
@@ -105,15 +134,37 @@ class Gate_Protocol():
         # orbit around center of gate while maintaining the gate in the center of our vision
         # OBJECT DETECTION
         # stop when the width of the gate reaches a maximum
-        previous_width = self.gate_width
-        position_around_circle = 0
-        while self.gate_width > previous_width:
-            previous_width = self.gate_width
-            position_around_circle += 1
-            # +++ controls: go to *self.set_points[position_around_circle]
-        # +++ controls: go to self.set_points[position_around_circle]
-        # PLEASE BE BLOCKING
-        pass
+        previous_width = self.gate_width        
+        position_around_circle = 1
+        # +++ controls: go to *self.set_points[position_around_circle%self.NUM_SET_POINTS]
+
+        # current_gate_width = self.gate_width
+        # starting_time = time.time()
+        # while current_gate_width == None:
+        #     current_gate_width = self.gate_width
+        #     current_time = time.time()
+        #     if current_time - starting_time > 5:
+        #         # +++ controls: go to *self.set_points[(position_around_circle+1)%self.NUM_SET_POINTS]
+        #         position_around_circle += 1
+        #         break
+        
+        while True:
+            starting_time = time.time()
+            while self.gate_width == None:
+                current_time = time.time()
+                if current_time - starting_time > 5:
+                    # +++ controls: go to *self.set_points[(position_around_circle+1)%self.NUM_SET_POINTS]
+                    position_around_circle += 1
+                    break
+                #wait a bit for the gate to not be none otherwise just go to the next position blind
+            if self.gate_width != None and self.gate_width > previous_width:
+                previous_width = self.gate_width
+                position_around_circle += 1
+                # +++ controls: go to *self.set_points[position_around_circle%self.NUM_SET_POINTS]
+            elif self.gate_width !=None and self.gate_width <= previous_width:
+                # +++ controls: go to *self.set_points[(position_around_circle-1)%self.NUM_SET_POINTS]
+                break
+        
 
     def forward(self):
         # OBJECT DETECTION
