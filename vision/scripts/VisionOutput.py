3d6af09e0d3a26bb877513f0328a978eae310b4d
echen4628 <echen4628@gmail.com>
2022-02-04 21:14:06 -0800

    add comments

--- vision/scripts/VisionOutput.py
+++ vision/scripts/VisionOutput.py
@@ -7,28 +7,12 @@ from std_msgs.msg import Header, String
 
 #range publishing template
 
-
-'''
-Header header
-string name
-float64 range
-float64 theta
-float64 phi
-float64 confidence
-vision_msgs/BoundingBox2D boundingBox
-'''
-'''
-obstacle1_header = Header()
-obstacle1_name = obstacle_name
-obstacle1_range = 1.0
-obstacle1_theta = 1.56
-obstacle1_phi = 0.2
-obstacle1_confidence = 0.8 #where 1 is 100%
-# bounding_box = BoundingBox2D(Pose2D(21.0, 21.0, 0.11), 51.0, 31.0)
-detection1 = Detection(obstacle1_header, obstacle1_name, obstacle1_range, obstacle1_theta, obstacle1_phi, obstacle1_confidence, bounding_box)
-visionPublisher.publishDetection(detection1)'''
 def callback(data):
-    #range estimation stuff happens
+    """Adds range estimation data to obstacle detection and publishes DetectionArray.
+
+    Args:
+        data: obstacle detection in the form of a BoundingBoxArray.msg.
+    """
     detectionList = []
     for currentBoundingBox in data.bounding_boxes:
         range = estimate_range(currentBoundingBox)
@@ -46,16 +30,32 @@ def callback(data):
     visionPublisher.publishDetectionArray(detectionArray)
 
 def estimate_range(boundingBox):
+    """Estimates range to an obstacle.
+
+    Args:
+        boundingBox: the boundingBox of the obstacle in the form of a BoundingBox.msg.
+
+    Return:
+        The distance to the obstacle in meters.
+    """
     return 15
 
 def estimate_angles(boundingBox):
-    '''returns theta, then returns phi in radians'''
+    """Estimates theta and phi of an obstacle relative to the camera.
+    
+    Args:
+        boundingBox: the boundingBox of the obstacle in the form of a BoundingBox.msg.
+
+    Return:
+        Theta is the horizontal, then phi is the vertical angle in radians.
+    """
     return 10, 20
     
 if __name__ == '__main__':
     rospy.init_node('vision_output')
-    visionPublisher = VisionPublisher("test_camera")
-    rospy.Subscriber("/vision/test_camera/bounding_box_array", BoundingBoxArray, callback)
+    visionPublisher = VisionPublisher("left_camera")
+    # rospy.Subscriber("/vision/test_camera/bounding_box_array", BoundingBoxArray, callback)
+    rospy.Subscriber("/vision/left_camera/bounding_box_array", BoundingBoxArray, callback)
     rospy.spin()
     
     
