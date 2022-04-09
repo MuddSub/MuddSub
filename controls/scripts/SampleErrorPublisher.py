58e6d0b5a10af3232f176075a3696196fc83fd39
echen4628 <echen4628@gmail.com>
2022-04-09 15:47:56 -0700

    sample error publisher to test gate state machine

--- controls/scripts/SampleErrorPublisher.py
+++ controls/scripts/SampleErrorPublisher.py
@@ -1,16 +1,17 @@
+#!/usr/bin/env python3
 import rospy
+from std_msgs.msg import Header, String
 from nav_msgs.msg import Odometry
-from geometry_msgs import Pose, PoseWithCovariance, TwitstWithCovariance, Twist, Point, Quaternion
+from geometry_msgs.msg import Pose, PoseWithCovariance, TwistWithCovariance, Twist, Point, Quaternion, Vector3
 import numpy as np
 from tf.transformations import quaternion_from_euler
 
 def publishOdometry():
-    pub = rospy.Publisher('/controls/robot/error',Odometry,self.error_callback,queue_size=10)
+    pub = rospy.Publisher('/controls/robot/error',Odometry, queue_size=10)
     rospy.init_node('SampleOdometry',anonymous=True)
     rate = rospy.Rate(10)
 
-    orientation = [0,0,np.pi/2]
-    orientation = quaternion_from_euler(orientation)
+    orientation = quaternion_from_euler(0,0,0)
 
     pose = Pose()
     pose.position = Point(0,0,0)
@@ -19,5 +20,20 @@ def publishOdometry():
     odometry = Odometry()
     odometry.pose = PoseWithCovariance(pose,[0.0]*36)
 
+    #
+    linear = Vector3(0.0,0.0,0.0)
+    angular = Vector3(0.0,0.0,0.0)
 
+    twist = Twist(linear,angular)
+    odometry.twist = TwistWithCovariance(twist,[0.0]*36)
 
+    odometry.header = Header()
+    odometry.child_frame_id = ""
+
+    pub.publish(odometry)
+    print("Published odometry")
+
+if __name__ == '__main__':
+    while not rospy.is_shutdown():
+        publishOdometry()
+        #rospy.spin()
--- mission/imports/mission/gate.py
+++ mission/imports/mission/gate.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from multiprocessing.dummy import active_children
 import rospy
 import smach
