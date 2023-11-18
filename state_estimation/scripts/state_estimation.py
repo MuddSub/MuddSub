546ff5f9c6ac8a9d37d1751a0d72cf6ff776c8ed
cplepel-py <cbplepel@gmail.com>
2023-11-18 12:32:00 -0800

    Added basic orientation correction

--- state_estimation/scripts/state_estimation.py
+++ state_estimation/scripts/state_estimation.py
@@ -5,6 +5,7 @@ import numpy as np
 import rospy
 from geometry_msgs.msg import PoseWithCovariance, Point, Pose, Quaternion
 from state_estimation.scripts.kalman import KinematicKalmanFilter
+from sensor_msgs.msg import Imu
 from drivers.msg import DVL
 
 
@@ -62,8 +63,13 @@ def exportPoseCov(pos: np.ndarray, cov: np.ndarray, orientation: np.ndarray) -> 
 
 def run_state_estimation():
     orientation = np.array([1, 0, 0, 0])
+    def set_orientation(new_orientation: np.ndarray):
+        nonlocal orientation
+        orientation = new_orientation
     filter = KinematicKalmanFilter(np.eye(3) * DRIFT, 3)
     dvl_sub = rospy.Subscriber("drivers/dvl", DVL, lambda m: filter.add_velocity(*parse_dvl(m)))
+    imu_sub = rospy.Subscriber("/vectornav/IMU", Imu,
+        lambda m: set_orientation(np.array([m.orientation.w, m.orientation.x, m.orientation.y, m.orientation.z])))
     pose_pub = rospy.Publisher("state/pose", PoseWithCovariance, queue_size=1)
     vel_pub = rospy.Publisher("state/velocity", PoseWithCovariance, queue_size=1)
     rospy.init_node("state_estimation", anonymous=False)
